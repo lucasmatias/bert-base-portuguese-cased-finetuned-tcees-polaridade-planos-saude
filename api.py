2001bd860da20a2b08e9c8ef878dcfe2fad4a064
Lucas Caetano <lucasmatiascaetano@gmail.com>
2025-03-26 21:54:39 -0300

    Update api.py

--- api.py
+++ api.py
@@ -4,21 +4,22 @@ from typing import List
 from gradio_client import Client
 
 app = FastAPI(
-    title="API para Classificação de Metas de Planos de Saúde",  # Novo título
+    title="Classificação de Metas de Planos de Saúde",
     description="API que classifica metas de planos de saúde em positivas ou negativas.",
     version="1.0.0",
-    openapi_url="https://digisusgmp.saude.gov.br/v1.5/docs/api-docs.json",
+    openapi_url="https://digisusgmp.saude.gov.br/v1.5/docs/api-docs.json",  # Altera a URL do JSON OpenAPI
     contact={
         "name": "Lucas Caetano",
         "url": "https://github.com/lucasmatias"
-    },    
+    },
     openapi_tags=[
         {"name": "Classificação Metas", "description": "Operações relacionadas à classificação de metas"}
-    ],
+    ],    
     servers=[
         {"url": "https://bert-base-portuguese-cased-finetuned.onrender.com", "description": "Servidor de Produção"},
-    ],    
+    ],   
 )
+
 client = Client("lucasmatias1990/bert-base-portuguese-cased-finetuned-tcees-polaridade-planos-saude")
 
 # Modelo da requisição
