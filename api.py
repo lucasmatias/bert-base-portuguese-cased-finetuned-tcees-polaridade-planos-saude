09d194081c261e4c17b5a7b4e74315935eae0d68
Lucas Caetano <lucasmatiascaetano@gmail.com>
2025-03-26 21:57:51 -0300

    Update api.py

--- api.py
+++ api.py
@@ -7,18 +7,21 @@ app = FastAPI(
     title="Classificação de Metas de Planos de Saúde",
     description="API que classifica metas de planos de saúde em positivas ou negativas.",
     version="1.0.0",
-    openapi_url="https://digisusgmp.saude.gov.br/v1.5/docs/api-docs.json",  # Altera a URL do JSON OpenAPI
+    openapi_url="/openapi.json",  # Mantém o JSON local da API
+    docs_url="/docs",  # Swagger UI
+    redoc_url="/redoc",  # ReDoc UI
     contact={
         "name": "Lucas Caetano",
         "url": "https://github.com/lucasmatias"
     },
     openapi_tags=[
         {"name": "Classificação Metas", "description": "Operações relacionadas à classificação de metas"}
-    ],    
+    ],
     servers=[
         {"url": "https://bert-base-portuguese-cased-finetuned.onrender.com", "description": "Servidor de Produção"},
-    ],   
+    ],
 )
+
 
 client = Client("lucasmatias1990/bert-base-portuguese-cased-finetuned-tcees-polaridade-planos-saude")
 
